dabaa4a1a298d2c4cf1730be530521b8bb2e0461
Mahshid Moghadasi <mm2994@cornell.edu>
2021-04-27 21:32:32 -0400

    Add files via upload

---
+++ Machina-Python/samples/machina_python_sample.py
@@ -0,0 +1,15 @@
+from machinaRobot import *
+
+def main():
+    bot = MachinaRobot()
+    bot.Message("Hello Robot!")
+    bot.SpeedTo(100)
+    bot.AxesTo(0,0,0,0,90,0)
+    bot.TransformTo(200,300,200,-1,0,0,0,1,0)
+    bot.Rotate(0,1,0,-90)
+    bot.Move(0,0,250)
+    bot.Wait(2000)
+    bot.AxesTo(0,0,0,0,90,0)
+
+if __name__ == "__main__":
+    main()
